e24d5974782405a3160138389deb79e79280c9d1
= <element.onex@gmail.com>
2019-07-06 02:17:53 +0300

    добавил первичную разметку и расположение элементов с помощью гридов

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://img.icons8.com/flat_round/64/000000/plus.png'></img>
       </header>
-    </div>
-  );
+      <nav className='nav'>
+          <div>Profile</div>
+          <div>Messages</div>
+          <div>News</div>
+          <div>Music</div>
+          <div>Settings</div>
+      </nav>
+      <div className='content'>
+          <img src='https://www.desktopbackground.org/p/2012/03/30/366906_unbelievable-wallpapers-wallpapers-cave_1920x1080_h.jpg'></img>
+      </div>
+    </div>);
 }
 
 export default App;
