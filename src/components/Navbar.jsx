e19fc685e61329ab1fd2ccbefb64dbd0a5091026
= <element.onex@gmail.com>
2019-07-08 13:21:25 +0300

    14: разбил App.css на модули, соответствующие компонентам

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
     return <nav className='nav'>
-        <div>Profile</div>
-        <div>Messages</div>
-        <div>News</div>
-        <div>Music</div>
-        <div>Settings</div>
+        <div className='item'>
+            <a>Profile</a>
+        </div>
+        <div className='item'>
+            <a>Messages</a>
+        </div>
+        <div className='item'>
+            <a>News</a>
+        </div>
+        <div className='item'>
+            <a>Music</a>
+        </div>
+        <div className='item'>
+            <a>Settings</a>
+        </div>
     </nav>;
 }
 
