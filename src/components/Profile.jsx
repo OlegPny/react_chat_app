0c25aeedac5c443f40bd80afe2ea17c3cc09505e
= <element.onex@gmail.com>
2019-07-10 00:01:02 +0300

    14: окончательно инкапсулировал стили в модули

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Profile.css';
+import classes from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={classes.content}>
         <div>
             <img src='https://www.desktopbackground.org/p/2012/03/30/366906_unbelievable-wallpapers-wallpapers-cave_1920x1080_h.jpg' alt=''></img>
         </div>
@@ -14,11 +14,11 @@ const Profile = () => {
           <div>
             New Post
         </div>
-            <div className='posts'>
-                <div className='item'>
+            <div className={classes.posts}>
+                <div className={classes.item}>
                     post1
                 </div>
-                <div className='item'>
+                <div className={classes.item}>
                     post2
                 </div>
             </div>
