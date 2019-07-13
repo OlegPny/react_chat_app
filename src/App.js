e08619ab1d64aac494b24d1191c6cd4a0d9ee72b
= <element.onex@gmail.com>
2019-07-13 17:21:20 +0300

    19: добавил компоненты роут, завернул все в <BrowserRouter>

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Profile from './components/profile/Profile';
 import Dialogs from './components/dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
     return (
-        <div className='app-wrapper' >
-            <Header />
-            <Navbar />
-            <div className='app-wrapper-content' >
-                <Dialogs />
-                {/* <Profile /> */}
-            </div>
-        </div >);
+        <BrowserRouter>
+            <div className='app-wrapper' >
+                <Header />
+                <Navbar />
+                <div className='app-wrapper-content' >
+                    <Route component={Dialogs} />
+                    <Route component={Profile} />
+                </div>
+            </div >
+        </BrowserRouter>
+    );
 }
 
 export default App;
