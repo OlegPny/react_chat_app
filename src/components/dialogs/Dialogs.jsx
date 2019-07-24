308d2ec020f94345d0f1108058c1eeee1198ea37
= <element.onex@gmail.com>
2019-07-25 00:57:24 +0300

    21: добавил страницу с диалогами и грид-разметку этой страницы

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
 
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className='dialog'>
+                    Юзер 1
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Юзер 2
+                </div>
+                <div className={s.dialog}>
+                    Юзер 3
+                </div>
+                <div className={s.dialog}>
+                    Юзер 4
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Кураж, епта, ты где?</div>
+                <div className={s.message}>Пора к милфам</div>
+            </div>
         </div>
     )
 }
