7bf963d8f9d1b81ae82d85abb9d7a73247065cbb
= <element.onex@gmail.com>
2019-08-24 00:25:54 +0300

    25: вынес компоненты DialogItem в массив

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const DialogItem = (props) => {
     return (
@@ -29,6 +29,12 @@ const Dialogs = (props) => {
 
     ]
 
+    let dialogsElements =  [
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>,
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>,
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+    ]
+
     let messagesData = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How are you?'},
@@ -40,16 +46,21 @@ const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+
+                {
+                    dialogsElements
+                }
+
+                {/*<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>*/}
+                {/*<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>*/}
+                {/*<DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>*/}
+                {/*<DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>*/}
+                {/*<DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>*/}
+                {/*<DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>*/}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {/*<Message message={messagesData[0].message}/>*/}
+                {/*<Message message={messagesData[1].message}/>*/}
             </div>
         </div>
     )
