5dfe2952dbc18f8e4b88134123c49db03f37b35c
= <element.onex@gmail.com>
2019-08-24 00:52:28 +0300

    25: добавил формирование компонент диалогов и сообщений с помощью map массива

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -29,12 +29,6 @@ const Dialogs = (props) => {
 
     ]
 
-    let dialogsElements =  [
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>,
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>,
-        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-    ]
-
     let messagesData = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How are you?'},
@@ -43,24 +37,17 @@ const Dialogs = (props) => {
         {id: 5, message: 'Yo'},
     ]
 
+    let dialogsElements = dialogsData.map(dialog => <DialogItem id={dialog.id} name={dialog.name}/>);
+
+    let messagesElements = messagesData.map(message => <Message message={message.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-
-                {
-                    dialogsElements
-                }
-
-                {/*<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>*/}
-                {/*<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>*/}
-                {/*<DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>*/}
-                {/*<DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>*/}
-                {/*<DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>*/}
-                {/*<DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>*/}
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                {/*<Message message={messagesData[0].message}/>*/}
-                {/*<Message message={messagesData[1].message}/>*/}
+                {messagesElements}
             </div>
         </div>
     )
