4ca829b90783ed0a5a7439449ee94c9ee457600c
= <element.onex@gmail.com>
2019-09-03 02:25:27 +0300

    27-29: вынес данные компоненты Dialogs в state.js

--- src/App.js
+++ src/App.js
@@ -6,25 +6,7 @@ import Profile from './components/profile/Profile';
 import Dialogs from './components/dialogs/Dialogs';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-const App = () => {
-
-    let dialogsData = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrew'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Viktor'},
-        {id: 6, name: 'Valera'},
-
-    ]
-
-    let messagesData = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'},
-    ]
+const App = (props) => {
 
     return (
         <BrowserRouter>
@@ -32,7 +14,9 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/messages' render={() => <Dialogs messagesData={messagesData} dialogsData={dialogsData}/>}/>
+                    <Route path='/messages'
+                           render={() => <Dialogs messagesData={props.state.dialogsState.messagesData}
+                                                  dialogsData={props.state.dialogsState.dialogsData}/>}/>
                     <Route path='/profile' component={Profile}/>
                 </div>
             </div>
---
+++ src/redux/state.js
@@ -0,0 +1,22 @@
+let state = {
+    dialogsState: {
+        dialogsData: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrew'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'},
+
+        ],
+        messagesData: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'},
+        ]
+    }
+};
+
+export default state;
