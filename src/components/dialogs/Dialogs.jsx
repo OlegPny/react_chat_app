4f3a8543e8e4b5e09a3f269f82e2692d1636631e
= <element.onex@gmail.com>
2019-09-04 23:16:49 +0300

    31: добавил ссылку на TextArea и колбек при нажатии кнопки Send

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -11,6 +11,14 @@ const Dialogs = (props) => {
     let messagesElements =
         props.dialogsState.messagesData.map(message => <Message message={message.message}/>)
 
+    let postRef = React.createRef();
+
+    let addPost = () => {
+        let text = postRef.current.value;
+        alert(text);
+    };
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -18,6 +26,12 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div>
+                <textarea ref={postRef}></textarea>
+            </div>
+            <div>
+                <button onClick={addPost}>Send</button>
             </div>
         </div>
     )
